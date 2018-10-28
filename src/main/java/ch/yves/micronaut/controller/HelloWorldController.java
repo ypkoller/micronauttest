3b90aa11863f442026de51a87e7e5b8a9c4cde7d
yves <yves@Harlekin-PC>
2018-10-28 17:19:42 +0100

    First commit

---
+++ src/main/java/ch/yves/micronaut/controller/HelloWorldController.java
@@ -0,0 +1,18 @@
+package ch.yves.micronaut.controller;
+
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Produces;
+
+
+@Controller("/hello")
+public class HelloWorldController {
+
+	 @Get("/") 
+	    @Produces(MediaType.TEXT_PLAIN) 
+	    public String index() {
+	        return "Hello World"; 
+	    }
+	
+}
---
+++ src/test/java/ch/yves/micronaut/controller/HelloWordControllerTest.java
@@ -0,0 +1,49 @@
+package ch.yves.micronaut.controller;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import io.micronaut.context.ApplicationContext;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.client.HttpClient;
+import io.micronaut.runtime.server.EmbeddedServer;
+
+//com.fasterxml.jackson.databind.ObjectMapper, com.fasterxml.jackson.databind.ObjectMapper
+public class HelloWordControllerTest {
+
+	 private static EmbeddedServer server;
+	    private static HttpClient client;
+
+	    @BeforeClass
+	    public static void setupServer() {
+	        server = ApplicationContext.run(EmbeddedServer.class); 
+	        client = server
+	                .getApplicationContext()
+	                .createBean(HttpClient.class, server.getURL());  
+	    }
+
+	    @AfterClass
+	    public static void stopServer() {
+	        if (server != null) {
+	            server.stop();
+	        }
+	        if (client != null) {
+	            client.stop();
+	        }
+	    }
+
+	    @Test
+	    public void testHello() throws Exception {
+	        HttpRequest request = HttpRequest.GET("/hello"); 
+	        String body = client.toBlocking().retrieve(request);
+	        assertNotNull(body);
+	        assertEquals(
+	                body,
+	                "Hello World"
+	        );
+	    }
+}
